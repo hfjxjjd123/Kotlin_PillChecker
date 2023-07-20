889b45b03634c881ef60769ca33b06a903edae12
hfjxjjd123 <licbusiness21@gmail.com>
2023-07-20 17:36:34 +0900

    Navigating: Calendar1 -> main

--- app/src/main/java/com/example/pill_checker/CalendarActivity1.kt
+++ app/src/main/java/com/example/pill_checker/CalendarActivity1.kt
@@ -1,11 +1,18 @@
 package com.example.pill_checker
 
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class CalendarActivity1:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar1)
+
+        val backArrow = findViewById<ImageButton>(R.id.back_to_main)
+        backArrow.setOnClickListener(){
+            finish()
+        }
+
     }
 }
