e9a086acdb082beb2123a13a900a9c6c6f7e95cd
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-01 14:35:51 +0900

    feat: Manage -> Update & Update -> BACK

--- app/src/main/java/com/example/pill_checker/ManageActivity.kt
+++ app/src/main/java/com/example/pill_checker/ManageActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pill_checker
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
@@ -8,13 +9,24 @@ import androidx.appcompat.app.AppCompatActivity
 class ManageActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val pillName = intent.getStringExtra("pillName")
+        val toUpdate = Intent(this, UpdateActivity::class.java)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage)
 
+        //NAVIGATION
         val backArrow = findViewById<ImageView>(R.id.back_arrow)
         backArrow.setOnClickListener(){
             finish()
+        }
+        val deleteButton = findViewById<ImageView>(R.id.delete_button)
+        deleteButton.setOnClickListener(){
+            finish()
+        }
+        val editButton = findViewById<ImageView>(R.id.edit_button)
+        editButton.setOnClickListener(){
+            toUpdate.putExtra("pillName", pillName)
+            startActivity(toUpdate)
         }
 
         val pillText = findViewById<TextView>(R.id.reg_name)
