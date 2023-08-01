b4757b4cc2309c7e9cb5214da4757af3066a62a4
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-01 14:26:29 +0900

    feat: Manage -> Back

--- app/src/main/java/com/example/pill_checker/ManageActivity.kt
+++ app/src/main/java/com/example/pill_checker/ManageActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pill_checker
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,7 +12,14 @@ class ManageActivity:AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage)
 
+        val backArrow = findViewById<ImageView>(R.id.back_arrow)
+        backArrow.setOnClickListener(){
+            finish()
+        }
+
         val pillText = findViewById<TextView>(R.id.reg_name)
         pillText.text = pillName
+
+
     }
 }
