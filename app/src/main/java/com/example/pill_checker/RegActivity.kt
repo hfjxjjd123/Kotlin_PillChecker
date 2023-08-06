062d034b584d11de07518a52011c9fd49434b980
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-06 16:42:58 +0900

    feat: 복용시간 탭시 색변경

--- app/src/main/java/com/example/pill_checker/RegActivity.kt
+++ app/src/main/java/com/example/pill_checker/RegActivity.kt
@@ -3,8 +3,14 @@ package com.example.pill_checker
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class RegActivity:AppCompatActivity() {
+    var morningOn = false
+    var lunchOn = false
+    var dinnerOn = false
+    var sleepOn = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
@@ -16,6 +22,47 @@ class RegActivity:AppCompatActivity() {
         }
         registerButton.setOnClickListener() {
             finish()
+        }
+
+        val morningClock = findViewById<Button>(R.id.morning_clock)
+        val lunchClock = findViewById<Button>(R.id.lunch_clock)
+        val dinnerClock = findViewById<Button>(R.id.dinner_clock)
+        val sleepClock = findViewById<Button>(R.id.sleep_clock)
+
+        val onColor = ContextCompat.getColor(this, R.color.primary)
+        val offColor = ContextCompat.getColor(this, R.color.primary_light)
+
+        morningClock.setOnClickListener() {
+            morningOn = !morningOn
+            if (morningOn) {
+                morningClock.setBackgroundColor(onColor)
+            } else {
+                morningClock.setBackgroundColor(offColor)
+            }
+        }
+        lunchClock.setOnClickListener() {
+            lunchOn = !lunchOn
+            if (lunchOn) {
+                lunchClock.setBackgroundColor(onColor)
+            } else {
+                lunchClock.setBackgroundColor(offColor)
+            }
+        }
+        dinnerClock.setOnClickListener() {
+            dinnerOn = !dinnerOn
+            if (dinnerOn) {
+                dinnerClock.setBackgroundColor(onColor)
+            } else {
+                dinnerClock.setBackgroundColor(offColor)
+            }
+        }
+        sleepClock.setOnClickListener() {
+            sleepOn = !sleepOn
+            if (sleepOn) {
+                sleepClock.setBackgroundColor(onColor)
+            } else {
+                sleepClock.setBackgroundColor(offColor)
+            }
         }
     }
 }
