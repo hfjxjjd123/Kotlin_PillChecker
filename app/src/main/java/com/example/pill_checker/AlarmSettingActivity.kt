b760af37a5bd3175eeb28fea44a3de67dedc3174
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-06 16:06:15 +0900

    Login2 Clock 설정

--- app/src/main/java/com/example/pill_checker/AlarmSettingActivity.kt
+++ app/src/main/java/com/example/pill_checker/AlarmSettingActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pill_checker
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 
 class AlarmSettingActivity:AppCompatActivity() {
@@ -12,6 +14,62 @@ class AlarmSettingActivity:AppCompatActivity() {
         val backArrow = findViewById<ImageButton>(R.id.back_arrow)
         backArrow.setOnClickListener(){
             finish()
+        }
+
+        val morningClock: Button = findViewById(R.id.morning_clock)
+
+        morningClock.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view) // Create a PopupMenu and pass the context and anchor view
+            popupMenu.inflate(R.menu.menu_morning_clock) // Inflate the menu resource
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                morningClock.text = item.title
+                true
+            }
+
+            popupMenu.show()
+        }
+
+        val lunchClock: Button = findViewById(R.id.lunch_clock)
+
+        lunchClock.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view) // Create a PopupMenu and pass the context and anchor view
+            popupMenu.inflate(R.menu.menu_lunch_clock) // Inflate the menu resource
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                lunchClock.text = item.title
+                true
+            }
+
+            popupMenu.show()
+        }
+
+        val dinnerClock: Button = findViewById(R.id.dinner_clock)
+
+        dinnerClock.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view) // Create a PopupMenu and pass the context and anchor view
+            popupMenu.inflate(R.menu.menu_dinner_clock) // Inflate the menu resource
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                dinnerClock.text = item.title
+                true
+            }
+
+            popupMenu.show()
+        }
+
+        val sleepClock: Button = findViewById(R.id.sleep_clock)
+
+        sleepClock.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view) // Create a PopupMenu and pass the context and anchor view
+            popupMenu.inflate(R.menu.menu_sleep_clock) // Inflate the menu resource
+
+            popupMenu.setOnMenuItemClickListener { item ->
+                sleepClock.text = item.title
+                true
+            }
+
+            popupMenu.show()
         }
 
     }
