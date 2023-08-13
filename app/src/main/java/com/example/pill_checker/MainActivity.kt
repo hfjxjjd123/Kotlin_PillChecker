a940697099dc65c32c592b9d1a171dc83160e921
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-13 17:15:49 +0900

    feat: ListUp 완료, not checked -> checed 순서 정렬 필요

--- app/src/main/java/com/example/pill_checker/MainActivity.kt
+++ app/src/main/java/com/example/pill_checker/MainActivity.kt
@@ -9,7 +9,10 @@ import android.widget.LinearLayout
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pill_checker.adapter.CheckRecyclerAdapter
 import com.example.pill_checker.adapter.PillOuterRecyclerAdapter
+import com.example.pill_checker.data.DoneItem
+import com.example.pill_checker.data.PillDone
 import com.example.pill_checker.data.PillItem
 
 var isLogin = false
@@ -17,6 +20,9 @@ var isLogin = false
 class MainActivity : AppCompatActivity() {
     private lateinit var outerRecyclerView: RecyclerView
     private lateinit var adapter: PillOuterRecyclerAdapter
+    private lateinit var checkRecyclerView: RecyclerView
+    private lateinit var checkAdapter: CheckRecyclerAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -42,6 +48,17 @@ class MainActivity : AppCompatActivity() {
         show_next.setOnClickListener(){
             //Panel Data Fetching
         }
+
+        val doneItems = listOf<PillDone>(
+            PillDone(1, "마그네슘", 0, "아침", "O"),
+            PillDone(2, "비타민", 0, "아침", "X"),
+            )
+
+        checkRecyclerView = findViewById<RecyclerView>(R.id.calendar_done_list)
+        checkRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        checkAdapter = CheckRecyclerAdapter(doneItems)
+        checkRecyclerView.adapter = checkAdapter
 
         val pills = listOf<PillItem>(
             PillItem(1),
--- app/src/main/java/com/example/pill_checker/adapter/CheckRecyclerAdapter.kt
+++ app/src/main/java/com/example/pill_checker/adapter/CheckRecyclerAdapter.kt
@@ -1,17 +1,17 @@
 package com.example.pill_checker.adapter
 
-import android.content.Intent
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.pill_checker.CalendarActivity2
 import com.example.pill_checker.R
-import com.example.pill_checker.data.DoneItem
+import com.example.pill_checker.data.PillDone
 
-class CheckRecyclerAdapter(private val items: List<DoneItem>) :
+class CheckRecyclerAdapter(private val items: List<PillDone>) :
     RecyclerView.Adapter<CheckRecyclerAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,22 +22,44 @@ class CheckRecyclerAdapter(private val items: List<DoneItem>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item: DoneItem = items[position]
+        val item: PillDone = items[position]
+
+        holder.pillName.text = item.name
 
         if (item.done == "O") {
-            holder.checkBox.isChecked = true
+            holder.checkImage.setImageResource(R.drawable.checkbox_custom_checked)
+            holder.pillName.paintFlags = holder.pillName.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
         } else{
-            holder.checkBox.isChecked = false
+            holder.checkImage.setImageResource(R.drawable.checkbox_custom)
+            holder.pillName.paintFlags =
+                holder.pillName.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
+
+        holder.pillTab.setOnClickListener {
+            if (item.done == "O") {
+                item.done = "X"
+                holder.checkImage.setImageResource(R.drawable.checkbox_custom)
+                holder.pillName.paintFlags =
+                    holder.pillName.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+            } else {
+                item.done = "O"
+                holder.checkImage.setImageResource(R.drawable.checkbox_custom_checked)
+                holder.pillName.paintFlags = holder.pillName.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+            }
+        }
+
     }
 
     override fun getItemCount(): Int {
         return items.size
     }
 
+
     // InnerViewHolder class definition
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val checkBox: CheckBox = itemView.findViewById<CheckBox>(R.id.check_box_done)
+        val pillTab: LinearLayout = itemView.findViewById<LinearLayout>(R.id.pill_done_layout)
+        val checkImage: ImageView = itemView.findViewById<ImageView>(R.id.pill_done_calendar)
+        val pillName: TextView = itemView.findViewById<TextView>(R.id.pill_name_calendar)
     }
 
 }
