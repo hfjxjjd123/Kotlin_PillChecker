b33b5ea6bf9cc8d1fab463b852e2f8961a7c7203
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-16 13:12:27 +0900

    feat: Update: 약 정보 불러오기

--- app/src/main/java/com/example/pill_checker/UpdateActivity.kt
+++ app/src/main/java/com/example/pill_checker/UpdateActivity.kt
@@ -10,101 +10,93 @@ import com.example.pill_checker.repo.PillRepo
 //TODO Update DB 로직 필요
 
 class UpdateActivity:AppCompatActivity() {
-    var morningOn = false
-    var lunchOn = false
-    var dinnerOn = false
-    var sleepOn = false
+    var time: Int = 0b0000
+    val pillRepo = PillRepo(MainDatabase.MainDatabase.getDatabase(this))
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val pid = intent.getLongExtra("pid", -1)
-
-        val pillRepo = PillRepo(MainDatabase.MainDatabase.getDatabase(this))
         val pill = pillRepo.getPillById(pid)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update)
 
         val pillText = findViewById<TextView>(R.id.reg_pill)
-        pillText.text = pill.name
-        val backArrow = findViewById<ImageButton>(R.id.back_arrow)
-        backArrow.setOnClickListener(){
-            finish()
-        }
-
         val morningClock = findViewById<Button>(R.id.morning_clock)
         val lunchClock = findViewById<Button>(R.id.lunch_clock)
         val dinnerClock = findViewById<Button>(R.id.dinner_clock)
         val sleepClock = findViewById<Button>(R.id.sleep_clock)
-
-        val onColor = ContextCompat.getColor(this, R.color.primary)
-        val offColor = ContextCompat.getColor(this, R.color.primary_light)
         val pillNum: Button = findViewById<Button>(R.id.pill_num)
         val pillImage = findViewById<ImageView>(R.id.pill_image)
 
-        //FIRST setting
-        if (pill.times and 0b0001 == 0b0001) {
-            morningOn = true
+        val backArrow = findViewById<ImageButton>(R.id.back_arrow)
+        backArrow.setOnClickListener(){
+
+            finish()
+        }
+
+
+
+        pillText.text = pill.name
+
+        val onColor = ContextCompat.getColor(this, R.color.primary)
+        val offColor = ContextCompat.getColor(this, R.color.primary_light)
+        time = pill.times
+        if (time and 0b0001 == 0b0001) {
             morningClock.setBackgroundColor(onColor)
         }
-        if (pill.times and 0b0010 == 0b0010) {
-            lunchOn = true
+        if (time and 0b0010 == 0b0010) {
             lunchClock.setBackgroundColor(onColor)
         }
-        if (pill.times and 0b0100 == 0b0100) {
-            dinnerOn = true
+        if (time and 0b0100 == 0b0100) {
             dinnerClock.setBackgroundColor(onColor)
         }
-        if (pill.times and 0b1000 == 0b1000) {
-            sleepOn = true
+        if (time and 0b1000 == 0b1000) {
             sleepClock.setBackgroundColor(onColor)
         }
 
         pillNum.text = when(pill.ea){
-            null -> "1.0"
+            null -> ""
             0 -> "0.5"
             1 -> "1.0"
             2 -> "1.5"
             3 -> "2.0"
-            else -> "0.0"
+            else -> "1.0"
         }
 
         pillImage.setImageBitmap(pill.image)
 
-        ///////////////////////////
-
         morningClock.setOnClickListener() {
-            morningOn = !morningOn
-            if (morningOn) {
+            time = time.xor(0b0001)
+            if (time and 0b0001 == 0b0001) {
                 morningClock.setBackgroundColor(onColor)
             } else {
                 morningClock.setBackgroundColor(offColor)
             }
         }
         lunchClock.setOnClickListener() {
-            lunchOn = !lunchOn
-            if (lunchOn) {
+            time = time.xor(0b0010)
+            if (time and 0b0010 == 0b0010) {
                 lunchClock.setBackgroundColor(onColor)
             } else {
                 lunchClock.setBackgroundColor(offColor)
             }
         }
         dinnerClock.setOnClickListener() {
-            dinnerOn = !dinnerOn
-            if (dinnerOn) {
+            time = time.xor(0b0100)
+            if (time and 0b0100 == 0b0100) {
                 dinnerClock.setBackgroundColor(onColor)
             } else {
                 dinnerClock.setBackgroundColor(offColor)
             }
         }
         sleepClock.setOnClickListener() {
-            sleepOn = !sleepOn
-            if (sleepOn) {
+            time = time.xor(0b1000)
+            if (time and 0b1000 == 0b1000) {
                 sleepClock.setBackgroundColor(onColor)
             } else {
                 sleepClock.setBackgroundColor(offColor)
             }
         }
-
         pillNum.setOnClickListener { view ->
             val popupMenu = PopupMenu(this, view) // Create a PopupMenu and pass the context and anchor view
             popupMenu.inflate(R.menu.menu_pill_num) // Inflate the menu resource
