b21a5f10241569110623a13d4bede6556f57e453
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-17 15:22:38 +0900

    Coroutine - CalendarActivity2 적용

--- app/src/main/java/com/example/pill_checker/CalendarActivity2.kt
+++ app/src/main/java/com/example/pill_checker/CalendarActivity2.kt
@@ -11,6 +11,7 @@ import com.example.pill_checker.dao.DateTimeManager
 import com.example.pill_checker.dao.MainDatabase
 import com.example.pill_checker.data.PillCheck
 import com.example.pill_checker.repo.PillCheckRepo
+import kotlinx.coroutines.*
 
 class CalendarActivity2 : AppCompatActivity() {
     private lateinit var calendarRecyclerView: RecyclerView
@@ -18,46 +19,57 @@ class CalendarActivity2 : AppCompatActivity() {
     private lateinit var db: MainDatabase
     private lateinit var pillCheckRepo: PillCheckRepo
 
+    lateinit var job: Job
+    private val coroutineContext = Dispatchers.Default + job
+
     override fun onCreate(savedInstanceState: Bundle?) {
         val date: Long = intent.getLongExtra("date", -1L)
         val time: Int = intent.getIntExtra("time", -1)
 
-        val dateDiff = DateTimeManager().getDateDiff(date)
+        job = Job()
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar2)
 
+        calendarRecyclerView = findViewById<RecyclerView>(R.id.calendar_panel)
+        calendarRecyclerView.layoutManager = LinearLayoutManager(parent.applicationContext)
+
         db = MainDatabase.getDatabase(applicationContext)
         pillCheckRepo = PillCheckRepo(db)
 
+        CoroutineScope(coroutineContext).launch {
+            val dateDiff = DateTimeManager().getDateDiff(date)
 
-        var text: String = ""
-        val calendarTimeText = findViewById<TextView>(R.id.calendar_text_time)
+            var text: String = ""
+            val calendarTimeText = findViewById<TextView>(R.id.calendar_text_time)
 
-        text += DateTimeManager().getDateBeforeString(dateDiff)
-        text += " "
-        text += DateTimeManager().getTimeString(time)
+            text += DateTimeManager().getDateBeforeString(dateDiff)
+            text += " "
+            text += DateTimeManager().getTimeString(time)
 
-        calendarTimeText.text = text
+            calendarTimeText.text = text
 
-        println("DEBUGING________________$text/$date/$time//////////////////////")
+            println("DEBUGING________________$text/$date/$time//////////////////////")
 
-        val backArrow = findViewById<ImageButton>(R.id.back_arrow)
-        backArrow.setOnClickListener() {
-            finish()
+            val backArrow = findViewById<ImageButton>(R.id.back_arrow)
+            backArrow.setOnClickListener() {
+                finish()
+            }
+
+            val checkItems = withContext(Dispatchers.IO){
+                pillCheckRepo.getPillChecksByDtid(date + time)
+            }
+            val alignedItems: MutableList<PillCheck> =
+                checkItems.sortedBy{ it.checked }.reversed().toMutableList()
+
+            calendarRecyclerAdapter = CheckRecyclerAdapter(parent.applicationContext, alignedItems)
+            calendarRecyclerView.adapter = calendarRecyclerAdapter
         }
 
-        ///
-        val checkItems = pillCheckRepo.getPillChecksByDtid(date + time)
-        val alignedItems: MutableList<PillCheck> =
-            checkItems.sortedBy{ it.checked }.reversed().toMutableList()
+    }
 
-        //doneItems 정렬된 상태로 넘겨줌
-        calendarRecyclerView = findViewById<RecyclerView>(R.id.calendar_panel)
-        calendarRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        calendarRecyclerAdapter = CheckRecyclerAdapter(this, alignedItems)
-        calendarRecyclerView.adapter = calendarRecyclerAdapter
-
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
