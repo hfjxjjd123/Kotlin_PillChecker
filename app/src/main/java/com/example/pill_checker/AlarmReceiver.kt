66a1eab7148600d8cd308a205b4d57fa84fd55e0
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-30 23:16:48 +0900

    feat: MorningAlarmReceiver 완성

--- app/src/main/java/com/example/pill_checker/AlarmReceiver.kt
+++ app/src/main/java/com/example/pill_checker/AlarmReceiver.kt
@@ -4,9 +4,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import androidx.core.content.edit
+import com.example.pill_checker.dao.DateTimeManager
+import com.example.pill_checker.dao.MainDatabase
+import com.example.pill_checker.repo.PillCheckRepo
+import com.example.pill_checker.repo.TimeRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class MorningAlarmReceiver : BroadcastReceiver(){
+
     override fun onReceive(context: Context?, intent: Intent?) {
         val sharedPreferences = context?.getSharedPreferences("EventLock", Context.MODE_PRIVATE)
         val morningLock = sharedPreferences?.getBoolean("morning", false)
@@ -15,6 +23,23 @@ class MorningAlarmReceiver : BroadcastReceiver(){
         sharedPreferences?.edit {
             putBoolean("sleep", false)
             putBoolean("morning", true)
+        }
+        if(context != null){
+            val db = MainDatabase.getDatabase(context.applicationContext)
+            val pillCheckRepo = PillCheckRepo(db)
+            val timeRepo = TimeRepo(db)
+            CoroutineScope(Dispatchers.IO).launch {
+                val tidNow = DateTimeManager.getDateTimeValueNow().and(0b0100).toInt()
+                val pillLightsNext = pillCheckRepo.getPillLightsByTid(tidNow)
+                if(pillLightsNext.isNotEmpty()){
+                    val consideredTid = timeRepo.pastLastTid(tidNow)
+                    if(consideredTid != null){
+                        val pillLights = pillCheckRepo.getPillLightsByTid(consideredTid)
+                        pillCheckRepo.pillLightToPillChecked(pillLights)
+                    }
+                    //TODO push notification
+                }
+            }
         }
     }
 }
