06ea83e33ee306afc8595342193e89ba5cfa0ad1
hfjxjjd123 <licbusiness21@gmail.com>
2023-08-30 16:03:38 +0900

    Event Occur 중복확인 & Unlock prev

--- app/src/main/java/com/example/pill_checker/AlarmReceiver.kt
+++ app/src/main/java/com/example/pill_checker/AlarmReceiver.kt
@@ -1,42 +1,58 @@
 package com.example.pill_checker
 
-import android.app.AlarmManager
-import android.app.Notification
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import androidx.core.app.NotificationCompat
-import com.example.pill_checker.dao.*
-import com.example.pill_checker.data.PillLight
-import com.example.pill_checker.repo.PillCheckRepo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.time.LocalDateTime
-import java.util.*
+import androidx.core.content.edit
 
 class MorningAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        val sharedPreferences = context?.getSharedPreferences("EventLock", Context.MODE_PRIVATE)
+        val morningLock = sharedPreferences?.getBoolean("morning", false)
+        if (morningLock == true) return
+        //Event 중복방지
+        sharedPreferences?.edit {
+            putBoolean("sleep", false)
+            putBoolean("morning", true)
+        }
     }
 }
 
 class LunchAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        val sharedPreferences = context?.getSharedPreferences("EventLock", Context.MODE_PRIVATE)
+        val lunchLock = sharedPreferences?.getBoolean("lunch", false)
+        if (lunchLock == true) return
+        //Event 중복방지
+        sharedPreferences?.edit {
+            putBoolean("morning", false)
+            putBoolean("lunch", true)
+        }
     }
 }
 
 class DinnerAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        val sharedPreferences = context?.getSharedPreferences("EventLock", Context.MODE_PRIVATE)
+        val dinnerLock = sharedPreferences?.getBoolean("dinner", false)
+        if (dinnerLock == true) return
+        //Event 중복방지
+        sharedPreferences?.edit {
+            putBoolean("lunch", false)
+            putBoolean("dinner", true)
+        }
     }
 }
 
 class SleepAlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        val sharedPreferences = context?.getSharedPreferences("EventLock", Context.MODE_PRIVATE)
+        val sleepLock = sharedPreferences?.getBoolean("sleep", false)
+        if (sleepLock == true) return
+        //Event 중복방지
+        sharedPreferences?.edit {
+            putBoolean("dinner", false)
+            putBoolean("sleep", true)
+        }
     }
 }
